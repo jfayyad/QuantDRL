65fddf0e25b37f58b3048a5d7bf8e2762c48d097
jfayyad <fayyad.jamil@gmail.com>
2023-02-27 14:04:28 -0800

    Dataset

---
+++ ResNet Implementation/Dataset.py
@@ -0,0 +1,19 @@
+import torch
+from torchvision import datasets, transforms
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        self.CIFAR = datasets.CIFAR10('./data/CIFAR', train=False, download=False, transform=transforms.Compose([
+                       transforms.Resize((224,224)),
+                       transforms.ToTensor(),
+                   ]))
+
+    def __getitem__(self, index):
+        data, target = self.CIFAR[index]
+
+        return data, target, index
+
+    def __len__(self):
+        return len(self.CIFAR)
+
+
